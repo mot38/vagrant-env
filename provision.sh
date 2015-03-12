654bffb0c90fef82acaca1b61ebb7fa5dc7f8907
unknown <cs045mt@LRID500239.diti.lr.net>
2015-03-12 12:51:55 +0000

    Adding Jenkins Code

--- provision.sh
+++ provision.sh
@@ -11,3 +11,12 @@ echo "Install Ruby Gem Bundler"
 echo "Install Python PIP"
   apt-get -y install python-pip python-dev build-essential
   pip install --upgrade virtualenv
+
+ 
+echo "Installing Jenkins"
+
+  wget -q -O - https://jenkins-ci.org/debian/jenkins-ci.org.key | sudo apt-key add -
+  sh -c 'echo deb http://pkg.jenkins-ci.org/debian binary/ > /etc/apt/sources.list.d/jenkins.list'
+  apt-get update
+  apt-get -qy install jenkins
+  service jenkins start
